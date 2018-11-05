d9ea962d91ce25488d16ccf800d81620662b28b1
Group 6 <thomas.jantos@rwth-aachen.de>
2018-11-05 17:33:48 +0100

    Fix segmentation faults and introduce the necessity for laser measurements

--- src/particlefilter.cpp
+++ src/particlefilter.cpp
@@ -17,7 +17,7 @@
 //#include <random>
 #include <stdlib.h>
 #include <cstdlib>
-#include <math.h> 
+#include <math.h>
 
 
 //For Gaussian noise: https://stackoverflow.com/questions/32889309/adding-gaussian-noise
@@ -47,8 +47,8 @@ public:
         double r2 = ((double) rand() / (RAND_MAX));
         double r3 = ((double) rand() / (RAND_MAX));
 
-        x = r1*0.5;  //r1*mapWidth*mapResolution/10.0; //10 since we know initial pose close to origin!
-        y = r2*0.5;  //r2*mapLength*mapResolution/10.0;
+        x = r1*1.5;  //r1*mapWidth*mapResolution/10.0; //10 since we know initial pose close to origin!
+        y = r2*1.5;  //r2*mapLength*mapResolution/10.0;
         theta = r3*2*M_PI; //Or does theta go from -pi to pi?
         m = 1.0/M;
     }
@@ -68,13 +68,19 @@ private:
     double range_max;
     double angle_min;
     double angle_increment;
+
+    bool map_received;
+    bool laser_scan_received;
+
     std::vector<float> measurements;
     float linear_x;
     float angular_z;
     nav_msgs::Odometry position_msg;
     nav_msgs::OccupancyGrid current_map;
     sensor_msgs::PointCloud pointcloud;
-    tf::TransformListener *tf_listener;
+    tf::TransformListener tf_listener;
+    std::vector <Particle> particles;
+
 
 public:
     ros::NodeHandle n;
@@ -88,133 +94,177 @@ public:
 
     PfPoseNode(){
         n = ros::NodeHandle("~");
+        initializeParticles();
 
-        position_pub = n.advertise<nav_msgs::Odometry>("particle_position",1);
+        map_received = false;
+        laser_scan_received = false;
+
+        position_pub = n.advertise<nav_msgs::Odometry>("/particle_position",1);
         measurement_sub = n.subscribe<sensor_msgs::LaserScan>("/scan",10,&PfPoseNode::readScan,this);
         gridmap_sub = n.subscribe<nav_msgs::OccupancyGrid>("/grid_map",1,&PfPoseNode::readMap,this);
         velocity_sub = n.subscribe<geometry_msgs::Twist>("/motor_controller/twist",1,&PfPoseNode::velocityCallback,this);
     }
 
+    void initializeParticles(){
+      for (int i = 0; i< M;i++){
+        Particle part;
+        //ROS_INFO("x: %f, y:%f, theta:%f,m:%f",part.x,part.y,part.theta, part.m);
+        particles.push_back(part);
+      }
+    }
+
 
     ///////////////////// POSITIONING LOOP (PARTICLE FILTER) ////////////////////////////////////////
-    void positioningLoop(std::vector<Particle> particles){   //Resamples and broadcasts new center of mass as best guess of pose
-        ROS_INFO("Running positioning loop.");
-        //ros::Time msg_time = ros::Time::now();
+    void positioningLoop(){   //Resamples and broadcasts new center of mass as best guess of pose
+      if(map_received){
+        if(laser_scan_received){
+          ROS_INFO("Running positioning loop.");
+          //ros::Time msg_time = ros::Time::now();
 
 
-        //Assign weights (importance factor) to particles
-        double CDF[M]; //Comulative distribution function for the weights
-        double cumsum = 0;
+          //Assign weights (importance factor) to particles
+          double CDF[M]; //Comulative distribution function for the weights
+          double cumsum = 0;
+          ROS_INFO("Get Weights of particles");
+          int i = 0;
+          for(std::vector<Particle>::iterator it = particles.begin(); it != particles.end(); ++it)
+          {
 
-        for(int i=0; i< M; i++)
-        {
-            particles[i].m = get_weight(particles[i]);  //updates weight
-            cumsum += (double) particles[i].m;   //use the weight for the CDF, double as to compare with r below
-            CDF[i] = cumsum;  //Not normalized, therefor use final cumsum to compute r below
+              it->m = get_weight(*it);  //updates weight
+              cumsum += (double) it->m;   //use the weight for the CDF, double as to compare with r below
+              CDF[i] = cumsum;  //Not normalized, therefor use final cumsum to compute r below
+              i += 1;
+          }
+
+          for(std::vector<Particle>::iterator it = particles.begin(); it != particles.end(); ++it){
+            //ROS_INFO("x: %f, y: %f, theta: %f, weight: %f",it->x,it->y,it->theta,it->m);
+          }
+          //Resample M new particles randomly according to weight
+  		    std::vector <Particle> new_particles(M);
+          int ind = 0;
+
+          //And move particles based on control input (speeds) with added noise
+          double dt = 1; //How to get time between particle updates??? Hardcode and tweek?
+          float v = linear_x;
+          float omega = angular_z;
+          double dx, dy;
+          if(omega == 0.0){
+            dx = v*dt;
+            dy = 0.0;
+          }
+          else{
+            dx = v/omega * sin(omega*dt) + v*dt;
+            dy = v/omega *( 1 - cos(omega*dt) );
+          }
+
+          //Define Gaussian noise   HELP: fix this!!!
+          const double mean = 0.0;
+          const double stddev = 0.01;
+          std::default_random_engine generator;
+          std::normal_distribution<double> dist(mean, stddev);
+          std::random_device rd{};
+          std::mt19937 gen{rd()};
+
+          std::normal_distribution<double> d{mean, stddev};
+          ROS_INFO("Resample");
+          int idx = 0;
+          for(std::vector<Particle>::iterator it = new_particles.begin(); it != new_particles.end(); ++it)
+          {
+              idx = 0;
+              double r = ((double) rand() * cumsum/ (RAND_MAX));
+              while(CDF[idx] < r)
+              {
+                  idx ++;
+              }
+              //ROS_INFO("Index: %i, Random: %f", idx,r);
+              it->x = particles[idx].x + dx*cos(particles[idx].theta) + d(gen); //Gaussian noise
+              it->y = particles[idx].y + dy*sin(particles[idx].theta) + d(gen);
+              it->theta = particles[idx].theta; //+ d(gen);
+          }
+
+          for(std::vector<Particle>::iterator it = new_particles.begin(); it != new_particles.end(); ++it){
+            ROS_INFO("x: %f, y: %f, theta: %f, weight: %f",it->x,it->y,it->theta,it->m);
+          }
+
+
+          //Check degeneracy:
+
+          //if almost all weight extremely small:
+          //resample
+
+          particles = new_particles; //Seems to work for vectors?//HELP: Pointers more efficient?
+
+  //        for(int i=0; i< M; i++){    //If above doesnt work
+   //           particles[i] = new_particles[i];
+    //      }
+
+          //The center of mass of the particles will give the best estimate of the position:
+
+          //WHAT TO DO IF PARTICLES SPRED OUT IN DIFFERENT BUNCHES?
+          double m_sum = 0;
+          double xm_sum = 0;
+          double ym_sum = 0;
+          double thetam_sum = 0;
+          ROS_INFO("Get Center of Mass");
+          for(std::vector<Particle>::iterator it = particles.begin(); it != particles.end(); ++it){
+              double x = it->x;
+              double y = it->y;
+              double theta = it->theta;
+              double m = it->m;
+              //ROS_INFO("x: %f, y: %f, theta:%f",x,y,theta);
+              m_sum += m;
+
+              xm_sum += x*m;
+              ym_sum += y*m;
+
+              if(theta > M_PI){      //Convert to interval -pi to pi for summing
+                theta -= 2*M_PI;
+              }
+              thetam_sum += theta*m;
+          }
+          //ROS_INFO("xm: %f, ym: %f, theta: %f", xm_sum,ym_sum,thetam_sum);
+          double x_cof = xm_sum/m_sum;
+          double y_cof = ym_sum/m_sum;
+          double theta_cof = thetam_sum/m_sum;
+          if(theta_cof<0){     //Convert back to intervall 0 to 2pi
+            theta_cof = 2*M_PI + theta_cof;
+          }
+          //Broadcast this as the estimated pose
+
+
+
+
+          //Publish the pf_pose over TF    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!!!!!!!
+
+          ROS_INFO("Publish values x: %f , y: %f and theta: %f",x_cof,y_cof,theta_cof);
+          geometry_msgs::Quaternion theta_quat = tf::createQuaternionMsgFromYaw(theta_cof);
+          /*
+                  geometry_msgs::TransformStamped pf_trans;
+                  pf_trans.header.stamp = msg_time;
+                  pf_trans.header.frame_id = "pf_pose";
+                  pf_trans.child_frame_id = "base_link";
+                  pf_trans.transform.translation.x = x_cof;
+                  pf_trans.transform.translation.y = y_cof;
+                  pf_trans.transform.translation.z = 0.0;
+
+                  pf_trans.transform.rotation = theta_quat;
+
+                  //pf_pose_broadcaster.sendTransform(pf_trans);
+                  //ROS_INFO("Position (x,y,phi) = (%f,%f,%f)", x,y,phi);
+                  */
+          position_msg.header.frame_id = "/map";
+          position_msg.pose.pose.position.x = x_cof;
+          position_msg.pose.pose.position.y = y_cof;
+          position_msg.pose.pose.position.z = 0;
+          position_msg.pose.pose.orientation = theta_quat;
+          position_msg.twist.twist.linear.x = linear_x; //example
+          position_msg.twist.twist.angular.z = angular_z; //example
+          position_pub.publish(position_msg);
+          ROS_INFO("Publishing finished");
+
         }
+      }
 
-        //Resample M new particles randomly according to weight
-		std::vector <Particle> new_particles(M);
-        int ind = 0;
-
-        //And move particles based on control input (speeds) with added noise
-        double dt = 1; //How to get time between particle updates??? Hardcode and tweek?
-        float v = linear_x;
-        float omega = angular_z;
-
-        double dx = v/omega * sin(omega*dt);
-        double dy = v/omega *( 1 - cos(omega*dt) );
-
-        //Define Gaussian noise   HELP: fix this!!!
-        //		const double mean = 0.0;
-        //		const double stddev = 0.01;
-        //		std::default_random_engine generator;
-        //		std::normal_distribution<double> dist(mean, stddev);
-        //    		std::random_device rd{};
-        //    		std::mt19937 gen{rd()};
-
-        //		std::normal_distribution<double> d{mean, stddev};
-
-        for(int i=0; i< M; i++)
-        {
-            double r = ((double) rand() * cumsum / (RAND_MAX));
-            while(CDF[ind] < r)
-            {
-                ind ++;
-            }
-            new_particles[i].x = particles[ind].x + dx;//+ d(gen); //Gaussian noise
-            new_particles[i].y = particles[ind].y + dy; //+ d(gen);
-            new_particles[i].theta = particles[ind].theta; //+ d(gen);
-        }
-
-
-        //Check degeneracy:
-
-        //if almost all weight extremely small:
-        //resample
-
-        particles = new_particles; //Seems to work for vectors?//HELP: Pointers more efficient?
-
-//        for(int i=0; i< M; i++){    //If above doesnt work
- //           particles[i] = new_particles[i];
-  //      }
-
-        //The center of mass of the particles will give the best estimate of the position:
-
-        //WHAT TO DO IF PARTICLES SPRED OUT IN DIFFERENT BUNCHES?
-        double m_sum = 0;
-        double xm_sum = 0;
-        double ym_sum = 0;
-        double thetam_sum = 0;
-
-        for(int i=0; i < M; i++){
-            double x = particles[i].x;
-            double y = particles[i].y;
-            double theta = particles[i].theta;
-            double m = particles[i].m;
-
-            m_sum += m;
-
-            xm_sum += x*m;
-            ym_sum += y*m;
-            thetam_sum += theta*m;
-
-        }
-
-        double x_cof = xm_sum/m_sum;
-        double y_cof = ym_sum/m_sum;
-        double theta_cof = thetam_sum/m_sum;
-        //Broadcast this as the estimated pose
-
-
-
-
-        //Publish the pf_pose over TF    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!!!!!!!
-
-
-        geometry_msgs::Quaternion theta_quat = tf::createQuaternionMsgFromYaw(theta_cof);
-        /*
-                geometry_msgs::TransformStamped pf_trans;
-                pf_trans.header.stamp = msg_time;
-                pf_trans.header.frame_id = "pf_pose";
-                pf_trans.child_frame_id = "base_link";
-                pf_trans.transform.translation.x = x_cof;
-                pf_trans.transform.translation.y = y_cof;
-                pf_trans.transform.translation.z = 0.0;
-
-                pf_trans.transform.rotation = theta_quat;
-
-                //pf_pose_broadcaster.sendTransform(pf_trans);
-                //ROS_INFO("Position (x,y,phi) = (%f,%f,%f)", x,y,phi);
-                */
-
-        position_msg.pose.pose.position.x = x_cof;
-        position_msg.pose.pose.position.y = y_cof;
-        position_msg.pose.pose.position.z = 0;
-        position_msg.pose.pose.orientation = theta_quat;
-        position_msg.twist.twist.linear.x = linear_x; //example
-        position_msg.twist.twist.angular.z = angular_z; //example
-        position_pub.publish(position_msg);
 
     }
 
@@ -227,6 +277,8 @@ public:
 
     void readMap(const nav_msgs::OccupancyGrid map_msg)
     {
+        ROS_INFO("Read Map");
+        map_received = true;
         current_map = map_msg;
         mapWidth = map_msg.info.width; // number of rows
         mapLength = map_msg.info.height; // number of columns
@@ -235,7 +287,7 @@ public:
 
     void velocityCallback(const geometry_msgs::Twist::ConstPtr& msg){
         //ROS_INFO("velocityCallback velocity linear: %f, velocity angular: %f", msg->linear.x,msg->angular.z);
-
+        ROS_INFO("Velocity callback");
         linear_x = (float) msg->linear.x;
         angular_z = (float) msg->angular.z;
         //I use both to move particles
@@ -250,13 +302,15 @@ public:
         angle_min = scan_msg.angle_min;
         angle_increment = scan_msg.angle_increment; // not needed anymore
         */
-
+        //ROS_INFO("Transform Laser scan");
+        laser_scan_received = true;
         laser_geometry::LaserProjection projector;
 
         try
         {
-            tf_listener->waitForTransform("/map","/laser",scan_msg.header.stamp+ros::Duration().fromSec(scan_msg.ranges.size()*scan_msg.time_increment),ros::Duration(2.0));
-            projector.transformLaserScanToPointCloud("/map",scan_msg,pointcloud,*tf_listener);
+            tf_listener.waitForTransform("/map","/laser",scan_msg.header.stamp+ros::Duration().fromSec(scan_msg.ranges.size()*scan_msg.time_increment),ros::Duration(2.0));
+            //ROS_INFO("Finished Listening");
+            projector.transformLaserScanToPointCloud("/map",scan_msg,pointcloud,tf_listener);
         }catch(tf::TransformException ex)
         {
             ROS_ERROR("Transform error in map node: %s", ex.what());
@@ -277,12 +331,17 @@ public:
         {
             x = particle.x + pointcloud.points[i].x; //IS THIS RIGHT??? Only if the point cload transform is to global coordinates
             y = particle.y + pointcloud.points[i].y;
-	
+
             map_index = round(x/mapResolution)*mapWidth+round(y/mapResolution); // map array cell index
-	
-			if(map_index >= current_map.data.size() ){probValue = 0;}   // Maybe to solve problem just below???
-            probValue = current_map.data[map_index]; // going to get index out of bounds all the time
-            //probValue = 0;
+
+			      if(map_index >= current_map.data.size() )
+            {
+              probValue = 0;
+            }
+            else{
+              probValue = current_map.data[map_index];
+            }
+
 
             if(probValue > 20){
                 valueSum += probValue;
@@ -318,7 +377,7 @@ public:
 int main(int argc, char **argv)
 {
     //Initialize the particles. The constructor does this UNIFORMLY!
-	std::vector <Particle> particles(M);
+
     ros::init(argc, argv,"pf_pose_est");
     PfPoseNode pf_pose_est;
 
@@ -326,11 +385,12 @@ int main(int argc, char **argv)
 
     while(ros::ok())
     {
-        pf_pose_est.positioningLoop(particles);
+        pf_pose_est.positioningLoop();
+        ROS_INFO("Test");
         ros::spinOnce();
         loop_rate.sleep();
+        ROS_INFO("The main loop");
     }
 
     return 0;
 }
-
